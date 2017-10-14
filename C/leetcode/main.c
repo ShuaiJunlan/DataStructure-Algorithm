1f7f4c8b44b692eb4ec620aece92e8e5445df1e5
Junlan Shuai <shuaijunlan@gmail.com>
2017-10-14 10:46:21 +0800

    Update files

---
+++ C/leetcode/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+int main()
+{
+    int nums1[] = {1};
+    int nums2[] = {2};
+    printf("%f \n", findMedianSortedArrays(nums1, 1, nums2, 1));
+    return 0;
+}
---
+++ C/leetcode/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+//  004
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size);
+
+#endif // MAIN_H_INCLUDED
