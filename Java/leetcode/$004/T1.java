0a11d73b803d5bc59a7cb70f761a9d5a24b64f4b
Junlan Shuai <shuaijunlan@gmail.com>
2017-10-14 10:50:11 +0800

    add 004th algorithm

---
+++ Java/leetcode/$004/T1.java
@@ -0,0 +1,36 @@
+package com.sh.leetcoder.$004;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 21:13 2017/10/13.
+ */
+class T1 {
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length, n = B.length;
+        int l = (m + n + 1) / 2;
+        int r = (m + n + 2) / 2;
+        return (getkth(A, 0, B, 0, l) + getkth(A, 0, B, 0, r)) / 2.0;
+    }
+
+    public double getkth(int[] A, int aStart, int[] B, int bStart, int k) {
+        if (aStart > A.length - 1) return B[bStart + k - 1];
+        if (bStart > B.length - 1) return A[aStart + k - 1];
+        if (k == 1) return Math.min(A[aStart], B[bStart]);
+
+        int aMid = Integer.MAX_VALUE, bMid = Integer.MAX_VALUE;
+        if (aStart + k/2 - 1 < A.length) aMid = A[aStart + k/2 - 1];
+        if (bStart + k/2 - 1 < B.length) bMid = B[bStart + k/2 - 1];
+
+        if (aMid < bMid)
+            return getkth(A, aStart + k/2, B, bStart,       k - k/2);// Check: aRight + bLeft
+        else
+            return getkth(A, aStart,       B, bStart + k/2, k - k/2);// Check: bRight + aLeft
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {1};
+        int[] nums2 = {2};
+        System.out.println(solution.getMidValueByTwo(nums1, nums2));
+    }
+}
