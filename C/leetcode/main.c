6b2cd7d1a04324a367cf1b2047259a510dad410d
Junlan Shuai <shuaijunlan@gmail.com>
2017-10-21 19:16:39 +0800

    Add 012 and 015 algorithm

--- C/leetcode/main.c
+++ C/leetcode/main.c
@@ -10,8 +10,23 @@ int main()
 
 
     // 008
+    /*
     char* a = "   -21";
     printf("%d \n", myAtoi(a));
+    */
+
+    // 012
+    /*
+    printf("%s \n", intToRoman1(9));
+    printf("%s \n", intToRoman1(99));
+
+    printf("%s \n", intToRoman(9));
+    printf("%s \n", intToRoman(99));
+    */
+
+    int a[] = {-1, 0, 1, 2, -1, -4};
+    int length = 12;
+    int** b = threeSum(a, 6, &length);
 
 
 
--- C/leetcode/main.h
+++ C/leetcode/main.h
@@ -10,4 +10,10 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
 //  008
 int myAtoi(char* str);
 
+//  A012
+char* intToRoman(int num);
+
+//  A015
+int** threeSum(int* nums, int numsSize, int* returnSize);
+
 #endif // MAIN_H_INCLUDED
