0f80de6f62b7247f4681a16212c3ed8547f0fdf1
Junlan Shuai <shuaijunlan@gmail.com>
2017-11-05 20:43:35 +0800

    add

---
+++ Java/leetcode/$028/Solution.java
@@ -0,0 +1,27 @@
+package $028;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 20:26 2017/11/5.
+ */
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null){
+            return -1;
+        }
+        for (int i = 0; ; i++){
+            for (int j = 0; ; j++){
+                if (j == needle.length()){
+                    return i;
+                }
+                if (i+j >= haystack.length()){
+                    return -1;
+                }
+                if (needle.charAt(j) != haystack.charAt(i+j)){
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+}
