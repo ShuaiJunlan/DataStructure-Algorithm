946de93ce2ff4952805d1a5038e0db8175b60cb3
shuaijunlan <shuaijunlan@gmail.com>
2018-04-17 15:54:12 +0800

    Swap Nodes in Pairs

---
+++ Java/leetcode/$024/Solution.java
@@ -0,0 +1,32 @@
+package $024;
+
+import datastruc.ListNode;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 14:55 2018/4/17.
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode node = head;
+
+        //the return head
+        head = head.next;
+        ListNode mid = node;
+        while (node != null && node.next != null){
+
+            mid.next = node.next;
+            mid = node;
+            //swap two adjacent nodes
+            ListNode temp = node.next.next;
+            node.next.next = node;
+            node.next = temp;
+            node = temp;
+
+        }
+        return head;
+    }
+}
