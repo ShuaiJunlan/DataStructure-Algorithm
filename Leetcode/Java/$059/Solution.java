5ae8bc2b1cef2dcefbca2d0231a9f00e88d73b9a
Shuai JUnlan <shuaijunlan@gmail.com>
2018-07-10 09:50:08 +0800

    Spiral Matrix II

---
+++ Leetcode/Java/$059/Solution.java
@@ -0,0 +1,35 @@
+package $059;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 9:35 AM 2018/07/10.
+ */
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        if (n <= 0){
+            return new int[0][];
+        }
+        int[][] matrix = new int[n][n];
+
+        int circle = 0, count = 0;
+        while (2 * circle < n) {
+            for (int i = circle; i < n - circle; i++){
+                matrix[circle][i] = ++count;
+            }
+            for (int j = circle + 1; j < n - circle - 1; j++){
+                matrix[j][n - circle - 1] = ++count;
+            }
+            for (int m = n - circle - 1; m >= circle && circle != n - circle - 1; m--){
+                matrix[n - circle - 1][m] = ++count;
+            }
+            for (int q = n - circle - 2; q > circle && circle != n - circle - 1; q--){
+                matrix[q][circle] = ++count;
+            }
+            circle++;
+        }
+        return matrix;
+    }
+}
