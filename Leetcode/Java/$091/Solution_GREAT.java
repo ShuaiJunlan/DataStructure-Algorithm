0ba926c9b655e68e9774e4a38f966afd889990c8
Shuai JUnlan <shuaijunlan@gmail.com>
2018-07-31 13:51:35 +0800

    update

--- Leetcode/Java/$091/Solution_GREAT.java
+++ Leetcode/Java/$091/Solution_GREAT.java
@@ -7,17 +7,19 @@ package $091;
 public class Solution_GREAT {
     public int numDecodings(String s) {
         final int len = s.length();
-        if (len == 0)
+        if (len == 0) {
             throw new IllegalArgumentException("s can't be empty");
+        }
 
         int pre = 1, cur = s.charAt(0) == '0' ? 0 : 1, tmp;
         for (int i = 1; i < len && cur != 0; ++i) {
             tmp = cur;
             if (s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s.charAt(i) <= '6')) {
-                if (s.charAt(i) == '0')
+                if (s.charAt(i) == '0'){
                     cur = pre;
-                else
+                } else {
                     cur += pre;
+                }
             } else if (s.charAt(i) == '0') {
                 cur = 0;
             }
