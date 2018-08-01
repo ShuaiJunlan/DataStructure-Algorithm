28adfade6e0ba0c7dce1808701be6fb14a24aca3
Shuai JUnlan <shuaijunlan@gmail.com>
2018-08-01 11:09:47 +0800

    update

--- Leetcode/Java/$043/Solution.java
+++ Leetcode/Java/$043/Solution.java
@@ -23,8 +23,10 @@ public class Solution {
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int p : pos) if(!(sb.length() == 0 && p == 0)){
-            sb.append(p);
+        for(int p : pos){
+            if(!(sb.length() == 0 && p == 0)){
+                sb.append(p);
+            }
         }
         return sb.length() == 0 ? "0" : sb.toString();
     }
