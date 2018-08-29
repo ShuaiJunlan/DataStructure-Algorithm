6597ad991edf2a301a62d3cda98aa224f2ecb8c1
shuaijunlan <shuaijunlan@gmail.com>
2018-08-29 20:58:42 +0800

    update

---
+++ Leetcode/Java/$122/Solution.java
@@ -0,0 +1,11 @@
+package $122;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 20:58 2018/8/29.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
