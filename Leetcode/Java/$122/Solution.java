e2185a99a8fcbbc5c2516700ce3f0036902be7b4
shuaijunlan <shuaijunlan@gmail.com>
2018-08-29 21:05:55 +0800

    update

--- Leetcode/Java/$122/Solution.java
+++ Leetcode/Java/$122/Solution.java
@@ -5,7 +5,13 @@ package $122;
  * @date Created on 20:58 2018/8/29.
  */
 public class Solution {
-    public static void main(String[] args) {
-
+    public int maxProfit(int[] prices){
+        int total = 0;
+        for (int i = 0; i < prices.length - 1; i++){
+            if (prices[i + 1] > prices[i]){
+                total += (prices[i+1] - prices[i]);
+            }
+        }
+        return total;
     }
 }
