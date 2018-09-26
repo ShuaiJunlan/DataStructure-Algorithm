b4caa0d301d0fc4148cf87bd27e6d020977a2cc2
Shuai Junlan <shuaijunlan@gmail.com>
2018-09-26 08:58:24 +0800

    Sum Root to Leaf Numbers

--- Leetcode/Java/$129/Solution.java
+++ Leetcode/Java/$129/Solution.java
@@ -14,23 +14,23 @@ public class Solution {
             return 0;
         }
         sum = 0;
-        dfs(root, root.val);
+        recurse(root, root.val);
         return sum;
     }
-    private void dfs(TreeNode root, int val){
+    private void recurse(TreeNode root, int val){
         if (root == null){
             return;
         }
         if (root.left == null && root.right == null){
             sum += val;
         } else if (root.left != null && root.right != null){
-            dfs(root.left, val * 10 + root.left.val);
-            dfs(root.right, val * 10 + root.right.val);
+            recurse(root.left, val * 10 + root.left.val);
+            recurse(root.right, val * 10 + root.right.val);
         }else if (root.left != null){
-            dfs(root.left, val * 10 + root.left.val);
+            recurse(root.left, val * 10 + root.left.val);
 
         }else{
-            dfs(root.right, val * 10 + root.right.val);
+            recurse(root.right, val * 10 + root.right.val);
         }
     }
 }
