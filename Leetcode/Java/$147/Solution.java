52c967b1b15101411be19878b14ad0c788681adc
Shuai Junlan <shuaijunlan@gmail.com>
2018-10-03 10:02:40 +0800

    Insertion Sort List

--- Leetcode/Java/$147/Solution.java
+++ Leetcode/Java/$147/Solution.java
@@ -11,15 +11,19 @@ public class Solution {
         if (null == head){
             throw  new NullPointerException("head shouldn't be null!");
         }
-        ListNode temp = head.next;
-        while (temp != null){
+        ListNode helper = new ListNode(0);
+        ListNode cur = head, pre = helper, next;
+        while (cur != null){
+            next = cur.next;
+            while (pre.next != null && pre.next.val < cur.val){
+                pre = pre.next;
+            }
 
+            cur.next = pre.next;
+            pre.next = cur;
+            pre = helper;
+            cur = next;
         }
-        return head;
-    }
-    private void swapVal(ListNode a, ListNode b){
-        int temp = a.val;
-        a.val = b.val;
-        b.val = temp;
+        return helper.next;
     }
 }
