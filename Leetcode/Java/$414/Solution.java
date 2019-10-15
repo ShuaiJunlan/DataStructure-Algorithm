40ae65ebe7683eb1ee874774b72a3df5071ccae6
Shuai Junlan <shuaijunlan@gmail.com>
2019-10-15 15:49:35 +0800

    Third Maximum Number

---
+++ Leetcode/Java/$414/Solution.java
@@ -0,0 +1,30 @@
+package $414;
+
+/**
+ * @author Shuai Junlan[shuaijunlan@gmail.com].
+ * @since Created in 3:33 PM 10/15/19.
+ */
+public class Solution {
+    public int thirdMax(int[] nums) {
+        if (nums == null || nums.length == 0){
+            throw new IllegalArgumentException("Illegal argument!");
+        }
+        Integer max1 = null, max2 = null, max3 = null;
+        for (Integer n : nums) {
+            if (n.equals(max1) || n.equals(max2) || n.equals(max3)) {
+                continue;
+            }
+            if (max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (max3 == null || n > max3) {
+                max3 = n;
+            }
+        }
+        return max3 == null ? max1 : max3;
+    }
+}
